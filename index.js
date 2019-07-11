e7993c7796d93a953ae9b27f888e1d0d2a587e97
Daria <bloczkaya98@bk.ru>
2019-07-11 16:08:48 +0300

    practice1

---
+++ index.js
@@ -0,0 +1,16 @@
+const http = require ('http');
+const fs = require ('fs');
+
+const server = http.createServer(function (request, responce) {
+    console.log(request.method, request.url);
+    if (request.url == '/'){
+       const content = fs.readFileSync ('practce1.html','utf8');
+       responce.end(content); 
+    }
+    else if (request.url == '/style.css'){
+        const content = fs.readFileSync ('style.css','utf8'); 
+        responce.end(content); 
+    }
+})
+server.listen(process.env.PORT || 3000 );
+console.log('server started');
