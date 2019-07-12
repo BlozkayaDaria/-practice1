14fa90de3e2ca5f8a135b6ce73b76d4d79f7f302
Daria <bloczkaya98@bk.ru>
2019-07-12 22:14:30 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const fs = require ('fs');
 const server = http.createServer(function (request, responce) {
     console.log(request.method, request.url);
     if (request.url == '/'){
-       const content = fs.readFileSync ('practce1.html','utf8');
+       const content = fs.readFileSync ('practice1.html','utf8');
        responce.end(content); 
     }
     else if (request.url == '/style.css'){
